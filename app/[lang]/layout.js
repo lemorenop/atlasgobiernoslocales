86623a77877d72f70f08e681a5719fec0da3858e
Agus <m.agustina.nahas@gmail.com>
2025-04-30 13:18:42 -0300

    shareeee

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -6,6 +6,7 @@ import "@/app/globals.css";
 import { Raleway } from "next/font/google";
 import GovernmentDataProvider from "./components/governmentDataProvider";
 import GAnalytics from "./components/analytics";
+import Head from 'next/head';
 
 const raleway = Raleway({
   subsets: ["latin"],
@@ -22,17 +23,30 @@ export const metadata = {
 export default async function RootLayout({ children, params }) {
   const { lang } = await params;
   return (
-    <html lang={lang}>
-      <body
-        className={`${raleway.className} antialiased min-h-screen flex flex-col justify-between bg-white font-normal font-[Raleway]`}
-      >
-      <GAnalytics/>
-      <GovernmentDataProvider lang={lang}>
-          <Navbar lang={lang} />
-          <div className="flex-grow">{children}</div>
-          <Footer lang={lang} />
-        </GovernmentDataProvider>
-      </body>
-    </html>
+    <>
+      <Head>
+        <meta property="og:title" content="CAF | Atlas de gobiernos locales y regionales de América Latina y el Caribe" />
+        <meta property="og:description" content="Explore, compare y descargue indicadores clave sobre el desarrollo en más de 18.000 jurisdicciones locales y regionales de América Latina y el Caribe. Una herramienta abierta y transparente para la toma de decisiones." />
+        <meta property="og:image" content="/share.png" />
+        <meta property="og:url" content="https://atlasgobiernoslocales.caf.com/es" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="CAF | Atlas de gobiernos locales y regionales de América Latina y el Caribe" />
+        <meta name="twitter:description" content="Explore, compare y descargue indicadores clave sobre el desarrollo en más de 18.000 jurisdicciones locales y regionales de América Latina y el Caribe. Una herramienta abierta y transparente para la toma de decisiones." />
+        <meta name="twitter:image" content="/share.png" />
+      </Head>
+      <html lang={lang}>
+        <body
+          className={`${raleway.className} antialiased min-h-screen flex flex-col justify-between bg-white font-normal font-[Raleway]`}
+        >
+        <GAnalytics/>
+        <GovernmentDataProvider lang={lang}>
+            <Navbar lang={lang} />
+            <div className="flex-grow">{children}</div>
+            <Footer lang={lang} />
+          </GovernmentDataProvider>
+        </body>
+      </html>
+    </>
   );
 }
