f2803d749125bfc5497a30ca0378cda406452cd4
lieta96 <julieta.garcia96@gmail.com>
2025-09-26 11:44:51 -0300

    google analytics added

--- app/[lang]/components/analytics.js
+++ app/[lang]/components/analytics.js
@@ -1,6 +1,31 @@
 "use client";
-import { GoogleAnalytics } from "nextjs-google-analytics";
+
+import Script from "next/script";
 
 export default function GAnalytics() {
-  return <GoogleAnalytics trackPageViews />;
+  const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+  if (!GA_ID) {
+    return null; // si no está definido, no carga nada
+  }
+
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+    </>
+  );
 }
